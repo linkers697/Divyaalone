db8d6ffe01482a123bc272d72f6dbe4e588c4818
linkers697 <music3fhhgyhfgh@gmail.com>
2025-10-02 08:46:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -48,7 +48,7 @@ API_KEY = getenv("API_KEY", '30DxNexGenBots8ea03d')
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/lexer697/AloneMusic",
+    "https://github.com/linkers697/Divyaalone",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
